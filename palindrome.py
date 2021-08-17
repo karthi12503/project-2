5bbf309e5dd8b43250e5cef00e0b7efdc89b98d7
Karthick <58014955+karthi12503@users.noreply.github.com>
2021-08-17 20:41:58 +0530

    Palindrome

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+number=str(input("enter the number:")
+reverse=number[::-1]
+if number == reverse:
+       print("The number",number,"is a PALINDROME")
+else:
+  	print("It is not a PALINDROME")
+
+
+
+
+
