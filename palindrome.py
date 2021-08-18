27d361c6634402eb808508f8ecdba77875b30e18
Karthick <58014955+karthi12503@users.noreply.github.com>
2021-08-18 06:34:42 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,9 +1,17 @@
-number=str(input("Enter the number"))
-reverse=number[::-1]
-if number == reverse:
-	print("The number",number,"is a PALINDROME")
-else:
- 	print("It is not a PALINDROME")
 
-
+def number():
+     number=str(input("enter the number:"))
+     reverse=number[::-1]
+     if number == reverse:
+           print("The number",number,"is a PALINDROME")
+     else:
+  	     print("It is not a PALINDROME")
+number()
+while True:
+	choise = int(input("If you want to use it again press '1' or press anyother number"))
+	if choise == 1:
+		number()
+	else:
+		print("THANK YOU (^_^)")
+		break
 
